0da36ebe04b256e7c7e1a75319fe5e4369bbf226
Cameron Pavey <cpave3@gmail.com>
2018-06-30 16:46:16 +1000

    :white_check_mark: Added unit tests for new helpers
      - ADD: mkdirp unit tests
      - ADD: initialiseLog unit tests
      - ADD: mockfs developer dependency

---
+++ tests/helpers.test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const mockfs = require('mock-fs')
+    , chai   = require('chai')
+    , assert = chai.assert
+    , expect = chai.expect
+    , fs     = require('fs');
+
+const helpers = require('../lib/helpers');
+
+describe('helpers.mkdirp', () => {
+    it('should be able to make a directory path recursively', () => {
+        mockfs({
+            'stagingDir': {}
+        });
+        const targetPath = './stagingDir/var/logs';
+        expect(fs.existsSync(targetPath)).to.equal(false);
+        helpers.mkdirp(targetPath);
+        expect(fs.existsSync(targetPath)).to.equal(true);
+        mockfs.restore();
+    });
+});
+
+describe('helpers.initialiseLog', () => {
+    it('should be able to create a log file at a valid directory', () => {
+        mockfs({
+            'stagingDir': {}
+        });
+        const targetFile = './stagingDir/sample.log';
+        expect(fs.existsSync(targetFile)).to.equal(false);
+        helpers.initialiseLog(targetFile);
+        expect(fs.existsSync(targetFile)).to.equal(true);
+        mockfs.restore();
+    });
+
+    it('should be able to create a log file at a non-existent directory', () => {
+        mockfs({
+            'stagingDir': {}
+        });
+        const targetFile = './stagingDir/newDir/sample.log';
+        expect(fs.existsSync(targetFile)).to.equal(false);
+        helpers.initialiseLog(targetFile);
+        expect(fs.existsSync(targetFile)).to.equal(true);
+        mockfs.restore();
+    });
+    
+    it('should fail when given an invalid directory', () => {
+        mockfs({
+            'stagingDir': {}
+        });
+        const targetFile = ['this isn\'t valid'];
+        expect(fs.existsSync(targetFile)).to.equal(false);
+        helpers.initialiseLog(targetFile);
+        expect(fs.existsSync(targetFile)).to.equal(false);
+        mockfs.restore();
+    });
+
+    it('should fail when given an null/undefined cosntructor value', () => {
+        mockfs({
+            'stagingDir': {}
+        });
+        const targetFile = ['this isn\'t valid'];
+        expect(helpers.initialiseLog()).to.equal(false);
+        mockfs.restore();
+    });
+    
+
+});
+
